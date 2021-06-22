5176a85006c6a1df44a1231577d64c97cfaf44f0
ONETEAM\JacobRushlow <JacobRushlow@truckstop.com>
2021-06-22 16:41:59 -0600

    Created CardComponent

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
+  @Input() song:Object = {};
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.song)
   }
 
 }
