c55c7d4e59772ca35a102735d995fef987412ce7
ONETEAM\JacobRushlow <JacobRushlow@truckstop.com>
2021-07-08 10:08:54 -0600

    Added a min-height and solved a few bugs

--- src/app/pages/results/results.component.ts
+++ src/app/pages/results/results.component.ts
@@ -12,11 +12,17 @@ export class ResultsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    this.data = history.state.data
-    this.input = history.state.input
-  }
-
-  ngOnChanges() {
-    this.data = history.state.data
+    const previousSongs = window.localStorage.getItem("last-search")
+    const previousInput = window.localStorage.getItem("last-input")
+    if (!history.state.data && previousSongs && previousInput) {
+      this.data = JSON.parse(previousSongs)
+      this.input = JSON.parse(previousInput)
+    }
+    else {
+      this.data = history.state.data
+      this.input = history.state.input 
+      window.localStorage.setItem("last-search", JSON.stringify(history.state.data)) 
+      window.localStorage.setItem("last-input", JSON.stringify(this.input))
+    }
   }
 }
